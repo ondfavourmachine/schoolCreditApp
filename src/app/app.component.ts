bfcd620031e2bd97eafb670dee827872e5a7efd8
Micheal <ondfavourmachine@gmail.com>
2021-05-27 12:52:47 -0200

    removed count down for a school

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -55,6 +55,11 @@ export class AppComponent
     router.events.subscribe(
       event => {
         if(event instanceof NavigationStart){
+          if(event.url.includes('greatgenesis')){
+            (document.querySelector('.overlay') as HTMLElement).style.display = 'none';
+            (document.querySelector('.overlay') as HTMLElement).style.zIndex = '-1';
+            clearInterval(this.intervalID);
+          }
           if(event.url.includes('user')){
             (document.querySelector('.overlay') as HTMLElement).style.display = 'none';
             (document.querySelector('.overlay') as HTMLElement).style.zIndex = '-1';
